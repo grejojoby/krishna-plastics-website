a450c284a92f039ef5516b42e788b68263798816
Grejo Joby <grejo00@gmail.com>
2020-11-27 19:35:14 +0530

    home page products

--- js/homeprojectSlider.js
+++ js/homeprojectSlider.js
@@ -9,11 +9,9 @@ var tempHome="";
 
 function CreateCardHome(data){
 
-    return `                <div class="project-item">
+    return `<div class="project-item">
     <img src="${data.imagePath}" alt="projects">
     <div class="overlay"></div>
-    <a href="#" class="view-icon img-popup" data-gall="project"> <i
-            class="fas fa-expand"></i></a>
     <div class="projects-content">
         <a href="#" class="category">${data.tag}</a>
         <h3><a href="#" class="tittle">${data.title}</a></h3>
